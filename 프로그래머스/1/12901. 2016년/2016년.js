5d5116d7da151fd5fc81bbec0bc92b23ec85b0e3
Dahyeon Jin <154739298+dahyeo-n@users.noreply.github.com>
2024-10-04 18:41:52 +0900

    [level 1] Title: 2016년, Time: 4.10 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,7 +1,4 @@
 function solution(a, b) {
-    const date = '2016-0'+ a + '-' + b;
     const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    const dayOfWeek = week[new Date(date).getDay()];
-    
-    return dayOfWeek;
+    return week[new Date(2016, a - 1, b).getDay()];
 }
