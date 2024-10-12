1dc5bb9fc59f6cfc8ae68a8cb37a18bbcc07916b
Dahyeon Jin <154739298+dahyeo-n@users.noreply.github.com>
2024-10-12 19:02:40 +0900

    [level 1] Title: 소수 찾기, Time: 159.38 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,23 @@
+function isPrime(x) {
+    if (x < 2) return false; // 1과 1보다 작은 수는 소수가 아님
+    if (x === 2) return true; // 2는 유일한 짝수 소수
+    if (x % 2 === 0) return false; // 그 외 짝수는 소수가 아님
+    
+    // 3부터 √x까지 홀수만 확인 (2는 이미 처리했으므로)
+    for (let i = 3; i <= Math.sqrt(x); i += 2) {
+        if (x % i === 0) return false;
+    }
+    
+    return true;
+}
+
+function solution(n) {
+    let answer = 0;
+    if (n >= 2) answer++; // 2는 유일한 짝수 소수이므로 따로 처리
+
+    for (let i = 3; i <= n; i += 2) { // 3부터 홀수만 검사
+        if (isPrime(i)) answer++;
+    }
+    
+    return answer;
+}
