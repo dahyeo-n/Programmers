11df2642bf01f8101f265ea55e849dd97ac7d36d
Dahyeon Jin <154739298+dahyeo-n@users.noreply.github.com>
2025-04-08 02:04:33 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,9 +1,16 @@
 function solution(array, commands) {
-    let answer = [];
-    for (let s = 0; s < commands.length; s++) {
-        const [i, j, k] = commands[s]
-        answer.push(array.slice(i - 1, j).sort((a, b) => a - b)[k - 1])
+    const answer = [];
+    
+    for (let i = 0; i < commands.length; i++) {
+        const slicedArray = array.slice(commands[i][0] - 1, commands[i][1]).sort((a, b) => a - b);
+        const number = slicedArray[commands[i][2] - 1];
+        answer.push(number);
     }
     
     return answer;
 }
+
+// 반복문을 돌려서 commands를 순회
+// array를 slice를 사용하여 자름
+// 자른 숫자 배열을 오름순으로 정렬
+// 배열의 n번째 숫자를 배열에 담음 (.push())
