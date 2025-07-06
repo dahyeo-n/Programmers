774535bef3a9e8cd1a0b1d58a791b6da7ad55313
Dahyeon Jin <154739298+dahyeo-n@users.noreply.github.com>
2025-07-06 23:30:33 +0900

    [level 1] Title: 콜라 문제, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -2,8 +2,8 @@ function solution(a, b, n) {
     let change = 0; // 받은 콜라 수
     
     while (n >= a) { // 교환 가능한 수량이 아닐 때까지 반복
-        change += Math.floor(n/a) * b;
-        n = Math.floor(n/a) * b + n % a;
+        change += Math.floor(n / a) * b;
+        n = Math.floor(n / a) * b + n % a;
     }
     
     return change;
