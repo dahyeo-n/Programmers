27333500a7e5d6db183de6d3861ddb89ad82a2e4
Dahyeon Jin <154739298+dahyeo-n@users.noreply.github.com>
2025-08-11 00:20:46 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -2,15 +2,17 @@ function solution(array, commands) {
     const answer = [];
     
     for (let i = 0; i < commands.length; i++) {
-        const slicedArray = array.slice(commands[i][0] - 1, commands[i][1]).sort((a, b) => a - b);
-        const number = slicedArray[commands[i][2] - 1];
-        answer.push(number);
+        const targetArr = commands[i];        
+        const sortedArr = array.slice(targetArr[0] - 1, targetArr[1]).sort((a, b) => a - b);
+        
+        answer.push(sortedArr.slice(targetArr[2] - 1, targetArr[2]).join(""));
     }
     
-    return answer;
+    return answer.map(Number);
 }
 
-// 반복문을 돌려서 commands를 순회
-// array를 slice를 사용하여 자름
-// 자른 숫자 배열을 오름순으로 정렬
-// 배열의 n번째 숫자를 배열에 담음 (.push())
+// commands 2차원 배열을 commands의 길이만큼 반복문을 돌림
+// commands의 2차원 배열의 첫 번째 및 두 번째 원소를 array.slice() 인자로 넘김
+// sort((a, b) => a - b)로 숫자 오름차순 정렬
+// commands의 세 번째 원소 위치에 있는 숫자를 answer 배열에 담음
+// Number로 타입 변환한 뒤 return
