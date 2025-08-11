e3a2ecf2c5f2b8c2d0f9321a7b97065abbe4c46d
Dahyeon Jin <154739298+dahyeo-n@users.noreply.github.com>
2025-08-11 21:36:01 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.91 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,12 +1,22 @@
 function solution(numbers) {
-    const temp = [];
+    const answer = [];
     
     for (let i = 0; i < numbers.length; i++) {
         for (let j = i + 1; j < numbers.length; j++) {
-            temp.push(numbers[i] + numbers[j])
+            const result = numbers[i] + numbers[j];
+            
+            if (!answer.includes(result)) {
+                answer.push(result);
+            }
         }
     }
     
-    const answer = [...new Set(temp)];
     return answer.sort((a, b) => a - b);
 }
+
+// 요구사항: 서로 다른 인덱스에 있는 2개의 수를 뽑아서 더한 값 중에 중복되지 않는 값만을 오름차순 정렬 후, return
+
+// 모든 배열의 요소를 반복해서 돌면서 값을 더함
+// 기존에 있는 수라면 answer 배열에 추가하지 않음
+// 새로운 수라면 answer 배열에 추가
+// 오름차순으로 정렬해서 return
