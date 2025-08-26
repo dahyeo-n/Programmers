39bc9e5d1b3182ccb31629833cac06674f7c470d
Dahyeon Jin <154739298+dahyeo-n@users.noreply.github.com>
2025-08-26 18:50:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.38 ms, Memory: 66.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,20 @@
 function solution(participant, completion) {
-  participant.sort();
-  completion.sort();
-
-  for (let i = 0; i < participant.length; i++) {
-    if (participant[i] !== completion[i]) {
-      return participant[i];
+    const map = new Map();
+    
+    // 참가자 인원 카운트
+    for (const name of participant) {
+        // 참가자 명단을 돌며 이름을 key로, 등장 횟수를 value로 저장
+        map.set(name, (map.get(name) || 0) + 1); // e.g. { leo: 2, kiki: 1, eden: 1 }
     }
-  }
+    
+    // 완주자 인원 차감
+    for (const name of completion) {
+        // 완주자 명단을 돌면서 같은 이름의 등장 횟수 차감
+        map.set(name, (map.get(name) - 1)); // e.g. { leo: 1, kiki: 0, eden: 0 }
+    }
+    
+    // 결국 값이 1로, 남은 이름이 완주하지 못한 참가자
+    for (const [name, count] of map.entries()) {
+        if (count > 0) return name;
+    }
 }
