8c1d9ee161ce6339c9eaca4a829c842cc2cfa750
Dahyeon Jin <154739298+dahyeo-n@users.noreply.github.com>
2025-08-27 14:00:02 +0900

    [level 2] Title: 올바른 괄호, Time: 2.54 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,12 +1,22 @@
 function solution(s) {
-  if (s[0] === ')') return false;
-  let number = 0;
-
-  for (let i = 0; i <= s.length - 1; i++) {
-    s[i] === '(' ? (number += 1) : (number -= 1);
-      
-    if (number < 0) return false;
-  }
-
-  return number === 0 ? true : false;
+    // 닫힌 괄호로 시작하면 false 반환
+    if (s[0] === ')') return false;
+    
+    // 열린/닫힌 괄호의 수에 따라 +/- 이행
+    let num = 0;
+    
+    // 괄호를 순회하며 괄호의 개수 계산
+    for (let i = 0; i < s.length; i++) {
+        // i가 열린 괄호면 num++, 닫힌 괄호면 num--
+        s[i] === '('
+            ? num += 1
+            : num -= 1;
+        
+        // 시점에 관계없이 닫힌 괄호가 더 많아지는 경우인 음수가 되면 false 반환
+        if (num < 0) return false;
+    }
+    
+    // num이 짝수라면 열린/닫힌 괄호의 수가 동일한 것이기에 true 반환,
+    // 홀수라면 올바르지 않은 괄호이기에 false 반환
+    return num / 2 === 0 ? true : false;
 }
