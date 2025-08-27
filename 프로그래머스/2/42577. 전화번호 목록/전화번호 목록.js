f17910b135e9569540ff4db2a2298c14b2e1e68a
Dahyeon Jin <154739298+dahyeo-n@users.noreply.github.com>
2025-08-27 13:00:12 +0900

    [level 2] Title: 전화번호 목록, Time: 154.00 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,11 +1,15 @@
 function solution(phone_book) {
-  phone_book.sort(); // 전화번호 리스트 정렬
-
-  for (let i = 0; i < phone_book.length - 1; i++) {
-    if (phone_book[i] === phone_book[i + 1].substring(0, phone_book[i].length)) {  // 현재 번호와 다음 번호의 시작 부분이 같으면 접두어
-      return false;
+    // 1. 전화번호부 정렬
+    phone_book.sort();
+    
+    // 2. 인접한 번호끼리 접두어 관계 확인
+    for (let i = 0; i < phone_book.length - 1; i++) {
+        // 다음 번호가 현재 번호를 접두어로 가지는지 확인
+        if (phone_book[i+1].startsWith(phone_book[i])) {
+            return false;
+        }
     }
-  }
-
-  return true;
+    
+    // 3. 접두어가 없으면 true
+    return true;
 }
