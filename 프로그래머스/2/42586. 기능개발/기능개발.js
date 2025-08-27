aea0412a5fadcca9f9e7bfe74eacfc8ec6036d3f
Dahyeon Jin <154739298+dahyeo-n@users.noreply.github.com>
2025-08-27 19:54:36 +0900

    [level 2] Title: 기능개발, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -3,11 +3,13 @@ function solution(progresses, speeds) {
     let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
     let maxDay = days[0];
 
-    for (let i = 0, j = 0; i< days.length; i++) {
+    // j는 answer 배열의 인덱스
+    for (let i = 0, j = 0; i < days.length; i++) {
         if (days[i] <= maxDay) {
             answer[j] += 1;
         } else {
             maxDay = days[i];
+            // j를 1 증가시켜서 새로운 인덱스로 이동한 뒤 answer[j]에 1을 넣음
             answer[++j] = 1;
         }
     }
